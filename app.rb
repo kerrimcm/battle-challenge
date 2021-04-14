7f77edf5b1929bdd07471fbf66d7ea3723ea2eba
Kerri McMahon <kerrimcm25@gmail.com>
2021-04-14 14:56:39 +0100

    Refactored using a POST-redirect loop

--- app.rb
+++ app.rb
@@ -2,17 +2,24 @@ require 'sinatra/base'
 require 'sinatra/reloader' 
 
 class Battle < Sinatra::Base
+  enable :sessions
   configure :development do
     register Sinatra::Reloader
   end
-    
+
   get '/' do
     erb :index
   end
 
   post '/names' do
-    @name_one = params[:name_one]
-    @name_two = params[:name_two]
+    session[:name_one] = params[:name_one]
+    session[:name_two] = params[:name_two]
+    redirect '/play'
+  end 
+
+  get '/play' do
+    @name_one = session[:name_one]
+    @name_two = session[:name_two]
     erb :play
   end 
 
